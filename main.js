3bf32d90626b73eb57a7402e387acbaf737fc2b2
lucidex <padulaguruge@gmail.com>
2020-06-29 22:27:33 +0530

    added nav bae

--- main.js
+++ main.js
@@ -1,23 +1,37 @@
 const electron = require('electron')
 const url = require('url')
-const path = require('path')
+const path = require('path');
 
 
-const {app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 
 let mainWindow
 // list for the app
 
-app.on('ready', function(){
+app.on('ready', function () {
     // create new window
     mainWindow = new BrowserWindow({});
-    
+
     // load html into window
 
     mainWindow.loadURL(url.format({
-        pathname:path.join(__dirname, 'mainWindow.html'),
-        protocol:'file',
-        slashes:true
-    })); 
+        pathname: path.join(__dirname, 'mainWindow.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+    // Build Menu
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    // insert menu
+    Menu.setApplicationMenu(mainMenu)
 });
+
+
+// create menu template
+
+const mainMenuTemplate = [
+    {
+        label: 'File'
+    }
+];
